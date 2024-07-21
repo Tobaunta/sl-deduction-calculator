223f4de263eb7681f3a620484b3b903e334435de
Pixel Pirates <tobauntat@gmail.com>
2024-07-21 17:50:35 +0200

    added school selection and dynamic ticket picker

--- script.js
+++ script.js
@@ -1,187 +1,209 @@
 elements = {
-    buttons: {
-        submit: document.getElementById("submit")
+  buttons: {
+    submit: document.getElementById("submit"),
+  },
+  inputs: {
+    fromDate: document.getElementById("fromdate"),
+    toDate: document.getElementById("todate"),
+    year: document.getElementById("year"),
+    price: document.getElementById("price"),
+    ticket: document.getElementById("ticket"),
+  },
+  locations: {
+    result: document.getElementById("result"),
+  },
+};
+
+priceMatrix = {
+  y2024: {
+    adult: {
+      d30: {
+        price: 1020,
+        reduction: 68,
+      },
+      d90: {
+        price: 2960,
+        reduction: 87,
+      },
+      y1: {
+        price: 10710,
+        reduction: 117,
+      },
+      arlanda30: {
+        price: 1430,
+        reduction: 95,
+      },
+      ul30: {
+        price: 1930,
+        reduction: 127,
+      },
     },
-    inputs: {
-        fromDate: document.getElementById("fromdate"),
-        toDate: document.getElementById("todate"),
-        year: document.getElementById("year"),
-        price: document.getElementById("price"),
-        ticket: document.getElementById("ticket")
+    reduced: {
+      d30: {
+        price: 650,
+        reduction: 43,
+      },
+      d90: {
+        price: 1880,
+        reduction: 55,
+      },
+      y1: {
+        price: 6830,
+        reduction: 75,
+      },
+      arlanda30: {
+        price: 1060,
+        reduction: 71,
+      },
+      ul30: {
+        price: 1290,
+        reduction: 81,
+      },
     },
-    locations: {
-        result: document.getElementById("result")
-    }
+    school: {
+      s90: {
+        price: 630,
+        reduction: 22,
+      },
+      f90: {
+        price: 770,
+        reduction: 27,
+      },
+      s120: {
+        price: 770,
+        reduction: 20,
+      },
+      f120: {
+        price: 830,
+        reduction: 22,
+      },
+      sht: {
+        price: 810,
+        reduction: 18,
+      },
+      fht: {
+        price: 900,
+        reduction: 19,
+      },
+      svt: {
+        price: 1020,
+        reduction: 19,
+      },
+      fvt: {
+        price: 1060,
+        reduction: 20,
+      },
+      sulht: {
+        price: 2360,
+        reduction: 61,
+      },
+      fulht: {
+        price: 1500,
+        reduction: 34,
+      },
+      sulvt: {
+        price: 3030,
+        reduction: 64,
+      },
+      fulvt: {
+        price: 1750,
+        reduction: 35,
+      },
+    },
+  },
+};
+
+elements.buttons.submit.addEventListener("click", getPrice);
+
+elements.inputs.price.addEventListener("change", listTickets);
+
+function getPrice() {
+  const fromDateInput = elements.inputs.fromDate;
+  const toDateInput = elements.inputs.toDate;
+  const yearInput = elements.inputs.year;
+  const priceInput = elements.inputs.price;
+  const ticketInput = elements.inputs.ticket;
+  let fromDate, toDate, diffDays, price, reduction, totalReduction, refund;
+
+  fromDate = new Date(fromDateInput.value);
+  toDate = new Date(toDateInput.value);
+  diffDays =
+    Math.round((toDate.getTime() - fromDate.getTime()) / (1000 * 3600 * 24)) +
+    1;
+
+  price =
+    priceMatrix[yearInput.value][priceInput.value][ticketInput.value].price;
+  reduction =
+    priceMatrix[yearInput.value][priceInput.value][ticketInput.value].reduction;
+  totalReduction = diffDays * reduction;
+  refund = price - totalReduction;
+  if (refund <= 0) {
+    refund = 0;
   }
-  
-  priceMatrix = {
-    y2024: {
-        adult: {
-            d30: {
-                price: 1020,
-                reduction: 68,
-            }, 
-            d90: {
-                price: 2960,
-                reduction: 87
-            }, 
-            y1: {
-                price: 10710,
-                reduction: 117
-            },
-            skol90: {
-                price: 630,
-                reduction: 22
-            }, 
-            fritid90: {
-                price: 770,
-                reduction: 27
-            }, 
-            skol120: {
-                price: 770,
-                reduction: 20
-            }, 
-            fritid120: {
-                price: 830,
-                reduction: 22
-            }, 
-            skolht: {
-                price: 810,
-                reduction: 18
-            }, 
-            fritidht: {
-                price: 900,
-                reduction: 19
-            }, 
-            skolvt: {
-                price: 1020,
-                reduction: 19
-            }, 
-            fritidvt: {
-                price: 1060,
-                reduction: 20
-            }, 
-            skolulht: {
-                price: 2360,
-                reduction: 61
-            }, 
-            fritidulht: {
-                price: 1500,
-                reduction: 34
-            }, 
-            skolulvt: {
-                price: 3030,
-                reduction: 64
-            }, 
-            fritidulvt: {
-                price: 1750,
-                reduction: 35
-            }, 
-            arlanda30: {
-                price: 1430,
-                reduction: 95
-            }, 
-            ul30: {
-                price: 1930,
-                reduction: 127
-            }
-        },
-        reduced: {
-            d30: {
-                price: 650,
-                reduction: 43
-            },
-            d90: {
-                price: 1880,
-                reduction: 55
-            },
-            y1: {
-                price: 6830,
-                reduction: 75
-            },
-            skol90: {
-                price: 630,
-                reduction: 22
-            }, 
-            fritid90: {
-                price: 770,
-                reduction: 27
-            }, 
-            skol120: {
-                price: 770,
-                reduction: 20
-            }, 
-            fritid120: {
-                price: 830,
-                reduction: 22
-            }, 
-            skolht: {
-                price: 810,
-                reduction: 18
-            }, 
-            fritidht: {
-                price: 900,
-                reduction: 19
-            }, 
-            skolvt: {
-                price: 1020,
-                reduction: 19
-            }, 
-            fritidvt: {
-                price: 1060,
-                reduction: 20
-            }, 
-            skolulht: {
-                price: 2360,
-                reduction: 61
-            }, 
-            fritidulht: {
-                price: 1500,
-                reduction: 34
-            }, 
-            skolulvt: {
-                price: 3030,
-                reduction: 64
-            }, 
-            fritidulvt: {
-                price: 1750,
-                reduction: 35
-            }, 
-            arlanda30: {
-                price: 1060,
-                reduction: 71
-            },
-            ul30: {
-                price: 1290,
-                reduction: 81
-            }
-        }
-    }
-  }
-  
-  elements.buttons.submit.addEventListener("click", getPrice);
-  
-  function getPrice() {
-    const fromDateInput = elements.inputs.fromDate;
-    const toDateInput = elements.inputs.toDate;
-    const yearInput = elements.inputs.year;
-    const priceInput = elements.inputs.price;
-    const ticketInput = elements.inputs.ticket;
-    let fromDate, toDate, diffDays, price, reduction, totalReduction, refund;
-  
-    fromDate = new Date(fromDateInput.value);
-    toDate = new Date(toDateInput.value);
-    diffDays = Math.round((toDate.getTime()-fromDate.getTime()) / (1000 * 3600 * 24)) + 1;
-  
-  
-    price = priceMatrix[yearInput.value][priceInput.value][ticketInput.value].price;
-    reduction = priceMatrix[yearInput.value][priceInput.value][ticketInput.value].reduction;
-    totalReduction = diffDays*reduction;
-    refund = price - totalReduction;
-    if(refund<=0){refund = 0}
-  
-    elements.locations.result.innerHTML = `<p>${fromDate.toISOString().substring(0, 10)} - ${toDate.toISOString().substring(0, 10)} = ${diffDays} dagar</p>
+
+  elements.locations.result.innerHTML = `<p>${fromDate
+    .toISOString()
+    .substring(0, 10)} - ${toDate
+    .toISOString()
+    .substring(0, 10)} = ${diffDays} dagar</p>
         <p>${diffDays} * ${reduction} = ${totalReduction}kr</p>
         <p>${price} - ${totalReduction} = ${refund}kr</p>`;
+}
+
+function listTickets() {
+  elements.inputs.ticket.options.length = 0;
+  if (
+    elements.inputs.price.value == "adult" ||
+    elements.inputs.price.value == "reduced"
+  ) {
+    elements.inputs.ticket.options.add(new Option("30-dagar", "d30"));
+    elements.inputs.ticket.options.add(new Option("90-dagar", "d90"));
+    elements.inputs.ticket.options.add(new Option("Års", "y1"));
+    elements.inputs.ticket.options.add(
+      new Option("30-dagars Arlanda", "arlanda30")
+    );
+    elements.inputs.ticket.options.add(new Option("30-dagars UL/SL", "ul30"));
+  } else if (elements.inputs.price.value == "school") {
+    elements.inputs.ticket.options.add(
+      new Option("Skolbiljett 90-dagar", "s90")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Fritidsbiljett 90-dagar", "f90")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Skolbiljett 120-dagar", "s120")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Fritidsbiljett 120-dagar", "f120")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Skolbiljett Hösttermin", "sht")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Fritidsbiljett Hösttermin", "fht")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Skolbiljett Vårtermin", "svt")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Fritidsbiljett Vårtermin", "fvt")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Skolbiljett UL/SL Hösttermin", "sulht")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Fritidsbiljett UL/SL Hösttermin", "fulht")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Skolbiljett UL/SL Vårtermin", "sulvt")
+    );
+    elements.inputs.ticket.options.add(
+      new Option("Fritidsbiljett UL/SL Vårtermin", "fulvt")
+    );
   }
-  
+}
+
+onload = function () {
+  listTickets();
+  elements.inputs.toDate.value = new Date().toISOString().substring(0, 10);
+}
